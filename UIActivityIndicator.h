b4dbf6984976fc5a8cd3d82006c9024a8e037ff2
Javier Torron Diaz <javier.torron.diaz@gmail.com>
2018-10-09 09:13:08 +0200

    Update UIActivityIndicator.h
    Adding MIT License

--- UIActivityIndicator.h
+++ UIActivityIndicator.h
@@ -3,7 +3,28 @@
 //
 //  Created by Javier Torrón Díaz on 19/9/18.
 //  Copyright © 2018 Javier Torrón Díaz. All rights reserved.
-//
+//  
+//  MIT License
+
+//  Copyright (c) 2018 Javier Torron Diaz
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
@@ -13,12 +34,12 @@
     UIImage *buttonImage;
     UIViewContentMode imageViewContentMode;
 }
-// ------------------- Propiedades
+// ------------------- Properties
 @property (nonatomic) IBInspectable UIColor *spinnerColor;
 @property (nonatomic) NSString *buttonTitle;
 @property (nonatomic, strong) UIActivityIndicatorView *spinner;
 @property (nonatomic) BOOL isLoading;
-// ------------------- Métodos
+// ------------------- Public Methods
 - (void) hideLoading;
 - (void) showLoading;
 @end
